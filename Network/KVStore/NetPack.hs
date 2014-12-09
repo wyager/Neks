6cbe5a207c2f056a50ae23b3c34800dc5a3bc52b
William Yager <will.yager@gmail.com>
2014-12-09 15:03:32 -0600

    working netRead with ExceptT, but it's gross

--- Network/KVStore/NetPack.hs
+++ Network/KVStore/NetPack.hs
@@ -10,10 +10,6 @@ import Control.Exception (catch)
 import System.IO (Handle)
 import Data.Word (Word64)
 
-import Control.Monad.Trans.Except (ExceptT(..), runExceptT, throwE, except)
-import Control.Monad.Trans.Class (lift)
-import Control.Monad (when)
-
 netWrite :: Handle -> ByteString -> IO ()
 netWrite hdl msg = do
 	let len = fromIntegral (BS.length msg) :: Word64
@@ -34,14 +30,3 @@ netRead hdl = read --`catch` handleWith (return (Left "Network read error"))
 				if BS.length dataBytes /= fromIntegral length
 					then return $ Left "Connection dropped during message read"
 					else return $ Right dataBytes
-
-netRead' :: Handle -> IO (Either String ByteString)
-netRead' hdl = runExceptT $ do
-		lengthBytes <- lift (hGet hdl 8)
-		when (BS.length lengthBytes /= 8) (throwE "Failed to read length")
-		length <- ExceptT (return (decode lengthBytes :: Either String Word64))
-		let length = fromIntegral length :: Int
-		when (length > 10^6) (throwE "Message is too long")
-		dataBytes <- lift (hGet hdl length)
-		when (BS.length dataBytes /= length) (throwE "Didn't receive entire message")
-		return dataBytes
