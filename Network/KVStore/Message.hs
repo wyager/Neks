b859f4145b785d427d48779ccd00824f3e13fc0c
William Yager <will.yager@gmail.com>
2014-12-09 22:28:31 -0600

    remove comments

--- Network/KVStore/Message.hs
+++ Network/KVStore/Message.hs
@@ -10,13 +10,6 @@ import Network.KVStore.Actions (Request(Set, Get), Reply(Found, NotFound))
 import Data.Serialize (encode, decode)
 import Data.MessagePack (Object(ObjectArray, ObjectInt, ObjectBinary))
 
---parseRequest :: ByteString -> Either String Request
---parseRequest bs = case decode bs of
---	Right (ObjectArray [ObjectInt 0, ObjectBinary k]) -> Right (Get k)
---	Right (ObjectArray [ObjectInt 1, ObjectBinary k, ObjectBinary v]) -> Right (Set k v)
---	Right x -> Left ("Invalid request structure: " ++ show x)
---	Left error -> Left ("Request decode error: " ++ error)
-
 parseRequests :: ByteString -> Either String [Request]
 parseRequests bs = case decode bs of
 	Right (ObjectArray requests) -> sequence (map parse requests)
@@ -26,25 +19,11 @@ parseRequests bs = case decode bs of
 		ObjectArray [ObjectInt 1, ObjectBinary k, ObjectBinary v] -> Right (Set k v)
 		x -> Left ("Invalid request structure: " ++ show x)
 
---formatRequest :: Request -> ByteString
---formatRequest (Set k v) = encode $ ObjectArray [ObjectInt 1,
---												ObjectBinary k,
---												ObjectBinary v]
---formatRequest (Get k) 	= encode $ ObjectArray [ObjectInt 0,
---												ObjectBinary k]
-
 formatRequests :: [Request] -> ByteString
 formatRequests = encode . ObjectArray . map (ObjectArray . format)
 	where format request = case request of
 		Set k v -> [ObjectInt 1, ObjectBinary k, ObjectBinary v]
 		Get k   -> [ObjectInt 0, ObjectBinary k]
-
---parseResponse :: ByteString -> Either String Reply
---parseResponse response = case decode response of
---	Right (ObjectArray [ObjectInt (-1), ObjectBinary k, ObjectBinary v]) -> Right (Found k v)
---	Right (ObjectArray [ObjectInt (-2), ObjectBinary k]) -> Right (NotFound k)
---	Right _ -> Left "Incorrect response type"
---	Left error -> Left ("Response decode error: " ++ error)
 
 parseResponses :: ByteString -> Either String [Reply]
 parseResponses bs = case decode bs of
@@ -54,13 +33,6 @@ parseResponses bs = case decode bs of
 		ObjectArray [ObjectInt (-1), ObjectBinary k, ObjectBinary v] -> Right (Found k v)
 		ObjectArray [ObjectInt (-2), ObjectBinary k] -> Right (NotFound k)
 		_ -> Left "Incorrect response type"
-
---formatResponse :: Reply -> ByteString
---formatResponse (Found k v)  = encode $ ObjectArray [ObjectInt (-1),
---												  	ObjectBinary k,
---												  	ObjectBinary v]
---formatResponse (NotFound k) = encode $ ObjectArray [ObjectInt (-2),
---													ObjectBinary k]
 
 formatResponses :: [Reply] -> ByteString
 formatResponses = encode . ObjectArray . map (ObjectArray . format)
