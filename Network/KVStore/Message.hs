d7b978c7ec74e651a49d9f85c32e194181214665
William Yager <will.yager@gmail.com>
2014-12-10 13:08:45 -0600

    changed message codec

--- Network/KVStore/Message.hs
+++ Network/KVStore/Message.hs
@@ -10,32 +10,31 @@ import Network.KVStore.Actions (Request(Set, Get), Reply(Found, NotFound))
 import Data.Serialize (encode, decode)
 import Data.MessagePack (Object(ObjectArray, ObjectInt, ObjectBinary))
 
-parseRequests :: ByteString -> Either String [Request]
-parseRequests bs = case decode bs of
-	Right (ObjectArray requests) -> sequence (map parse requests)
-	Left error -> Left ("Request decode error: " ++ error)
-	where parse request = case request of
-		ObjectArray [ObjectInt 0, ObjectBinary k] -> Right (Get k)
-		ObjectArray [ObjectInt 1, ObjectBinary k, ObjectBinary v] -> Right (Set k v)
-		x -> Left ("Invalid request structure: " ++ show x)
-
 formatRequests :: [Request] -> ByteString
 formatRequests = encode . ObjectArray . map (ObjectArray . format)
 	where format request = case request of
 		Set k v -> [ObjectInt 1, ObjectBinary k, ObjectBinary v]
 		Get k   -> [ObjectInt 0, ObjectBinary k]
 
-parseResponses :: ByteString -> Either String [Reply]
-parseResponses bs = case decode bs of
-	Right (ObjectArray responses) -> sequence (map parse responses)
-	Left error -> Left ("Response decode error: " ++ error)
-	where parse response = case response of
-		ObjectArray [ObjectInt (-1), ObjectBinary k, ObjectBinary v] -> Right (Found k v)
-		ObjectArray [ObjectInt (-2), ObjectBinary k] -> Right (NotFound k)
-		_ -> Left "Incorrect response type"
-
 formatResponses :: [Reply] -> ByteString
 formatResponses = encode . ObjectArray . map (ObjectArray . format)
 	where format response = case response of
 		Found k v  -> [ObjectInt (-1), ObjectBinary k, ObjectBinary v]
 		NotFound k -> [ObjectInt (-2), ObjectBinary k]
+
+parseRequests :: ByteString -> Either String [Request]
+parseRequests = parse request
+request (ObjectArray [ObjectInt 0, ObjectBinary k]) = Right (Get k)
+request (ObjectArray [ObjectInt 1, ObjectBinary k, ObjectBinary v]) = Right (Set k v)
+request _ = Left "Invalid request structure"
+
+parseResponses :: ByteString -> Either String [Reply]
+parseResponses = parse response
+response (ObjectArray [ObjectInt (-1), ObjectBinary k, ObjectBinary v]) = Right (Found k v)
+response (ObjectArray [ObjectInt (-2), ObjectBinary k]) = Right (NotFound k)
+response _ = Left "Incorrect response type"
+
+parse :: (Object -> Either String a) -> ByteString -> Either String [a]
+parse parser bs = case decode bs of
+	Right (ObjectArray responses) -> sequence (map parser responses)
+	error -> Left "Response decode failure"
